5a4ae68637d15ccc987a38f7c2c2863ac37fe4d8
hunnania <alifa.hanania@ui.ac.id>
2023-09-22 14:30:35 +0700

    implemented login, logout, cookies, and added more attributes

--- main/forms.py
+++ main/forms.py
@@ -4,4 +4,4 @@ from main.models import Product
 class ProductForm(ModelForm):
     class Meta:
         model = Product
-        fields = ["name", "amount", "description"]
+        fields = ["name", "amount", "price", "description"]
--- main/models.py
+++ main/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     date_added = models.DateField(auto_now_add=True)
     amount = models.IntegerField()
+    price = models.CharField(max_length=255)
     description = models.TextField()
