3d7a29611bc6370d2cebff31a2eeb22d3ddf4dad
hunnania <alifa.hanania@ui.ac.id>
2023-11-22 11:03:34 +0700

    fixed tugas 9

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseNotFound, HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseNotFound, HttpResponseRedirect, HttpResponse, JsonResponse
 from django.core import serializers
 from django.urls import reverse
 from django.contrib import messages  
@@ -10,6 +10,7 @@ from django.views.decorators.csrf import csrf_exempt
 from main.forms import ProductForm
 from main.models import Product
 import datetime
+import json
 
 # Create your views here.
 @login_required(login_url='/login')
@@ -127,3 +128,23 @@ def add_product_ajax(request):
         return HttpResponse(b"CREATED", status=201)
 
     return HttpResponseNotFound()
+
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_product = Product.objects.create(
+            user = request.user,
+            name = data["name"],
+            amount = int(data["amount"]),
+            price = int(data["price"]),
+            description = data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
